606f753488ddc635bbe3cdc47d08869278bdddca
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-04-22 22:27:38 -0300

    I forgot to close the connection to the database

--- q3_angeloBarcelos/q3_angeloBarcelos.py
+++ q3_angeloBarcelos/q3_angeloBarcelos.py
@@ -6,22 +6,27 @@ class Database:
         self.conn = None
 
     connect = lambda self: self.conn if hasattr(self, 'conn') and self.conn is not None else (
-        print("Connecting to the database...") or setattr(self, 'conn', psycopg2.connect(**self.conn_params)) or self.conn if hasattr(self, 'conn_params') else print("Error: 'conn_params' attribute not found.")
+        print("Conectando ao banco de dados...") or setattr(self, 'conn', psycopg2.connect(**self.conn_params)) or self.conn if hasattr(self, 'conn_params') else print("Erro: atributo 'conn_params' não encontrado.")
     )
+
     disconnect = lambda self: self.conn.close() if self.conn else None
 
     def execute(self, query, args=None):
-            connect_if_needed = lambda: self.connect() if not self.conn else None
-            connect_if_needed()
-            cursor = lambda: self.conn.cursor()
-            cursor = cursor()
-            cursor.execute(query, args)
-            return cursor
+        connect_if_needed = lambda: self.connect() if not self.conn else None
+        connect_if_needed()
+        cursor = lambda: self.conn.cursor()
+        cursor = cursor()
+        cursor.execute(query, args)
+        return cursor
 
     insert = lambda self, table, fields, values: (lambda query, values: (self.execute(query, values), self.conn.commit()))(f"INSERT INTO {table} ({', '.join(fields)}) VALUES ({', '.join(['%s'] * len(values))})", values)
+
     get_all = lambda self, table: (lambda query: self.execute(query).fetchall())(f"SELECT * FROM {table}")
+
     get_by_id = lambda self, table, id: (lambda query, id: self.execute(query, (id,)).fetchone())(f"SELECT * FROM {table} WHERE id = %s", id)
+
     update = lambda self, table, id, fields, values: (lambda query, id, values: (self.execute(query, (*values, id)), self.conn.commit()))(f"UPDATE {table} SET {', '.join([f'{field}=%s' for field in fields])} WHERE id = %s", id, values)
+
     delete = lambda self, table, id: (lambda query, id: (self.execute(query, (id,)), self.conn.commit()))(f"DELETE FROM {table} WHERE id = %s", id)
 
 try:
@@ -34,12 +39,19 @@ try:
 
     db = lambda: Database(conn_params("localhost", "AV2", "root", "123"))
     db = db()
+
     db.insert("USERS", ["id", "name", "country", "id_console"], [1, "Angelo Rodrigues", "Brasil", 2])
     print(db.get_all("USERS"))
     print(db.get_by_id("USERS", 1))
+
     db.update("USERS", 1, ["name"], ["Angelo Barcelos"])
     print(db.get_by_id("USERS", 1))
+
     db.delete("USERS", 1)
     print(db.get_all("USERS"))
+
 except Exception as e:
     print(f"Erro geral: {e}")
+
+finally:
+    db.disconnect()
